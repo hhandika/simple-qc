548cf59c9baa3fc1fe71552db27cfa04b120cbc8
Heru Handika <herubiolog@gmail.com>
2020-12-31 19:17:34 -0600

    Clean code. Indented long string and add gap in match methods.

--- src/parser.rs
+++ src/parser.rs
@@ -32,19 +32,26 @@ pub fn parse_fastq_gz(input: &PathBuf) -> Summary {
         .for_each(|(idx, recs)|
             match idx % 4 {
                 0 => { if !&recs.starts_with('@') {
-                            panic!("{:?} IS INVALID FASTQ. LOOKING FOR '@' FOUND '{}' at line {}",
+                            panic!("{:?} IS INVALID FASTQ. \
+                                LOOKING FOR '@' FOUND '{}' at line {}",
                                 &input, &recs, &idx + 1);
                         } else { reads += 1 }},
-                1 => sq_per_read.push(SeqReads::count_reads(&recs)), 
+
+                1 => sq_per_read.push(SeqReads::count_reads(&recs)),
+                
                 2 => { if !&recs.starts_with('+') {
-                            panic!("{:?} IS INVALID FASTQ. LOOKING FOR '+' FOUND '{}' at line {}",
+                            panic!("{:?} IS INVALID FASTQ. \
+                                LOOKING FOR '+' FOUND '{}' at line {}",
                                 &input, &recs, &idx + 1);
-                    }},
+                        }},
+
                 3 => qscores.push(QScore::analyze_qscores(&recs.as_bytes())),
+
                 _ => panic!("INVALID FASTQ!"),
             });
 
-    let all_reads: Summary = Summary::count_all_reads(&input, &reads, &sq_per_read, &qscores);
+    let all_reads = Summary::count_all_reads(
+                        &input, &reads, &sq_per_read, &qscores);
     writeln!(outbuff, "\x1b[0;32mDONE!\x1b[0m").unwrap();
     all_reads
 }
@@ -62,14 +69,16 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "\"test_files/invalid.fastq.gz\" IS INVALID FASTQ. LOOKING FOR '+' FOUND '-' at line 3")]
+    #[should_panic(expected = "\"test_files/invalid.fastq.gz\" IS INVALID FASTQ. \
+                                LOOKING FOR '+' FOUND '-' at line 3")]
     fn test_parsing_invplus_panic() {
         let input = PathBuf::from("test_files/invalid.fastq.gz");
         parse_fastq_gz(&input);
     }
 
     #[test]
-    #[should_panic(expected = "\"test_files/invalid2.fastq.gz\" IS INVALID FASTQ. LOOKING FOR '@' FOUND 'Bunomys_chrysocomus' at line 9")]
+    #[should_panic(expected = "\"test_files/invalid2.fastq.gz\" IS INVALID FASTQ. \
+                                LOOKING FOR '@' FOUND 'Bunomys_chrysocomus' at line 9")]
     fn test_parsing_invname_panic() {
         let input = PathBuf::from("test_files/invalid2.fastq.gz");
         parse_fastq_gz(&input);
