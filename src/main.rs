074630c43a4cd8cc4d7e18a06dba6f5fc8161c59
Heru Handika <herubiolog@gmail.com>
2021-01-01 11:47:24 -0600

    Update to version 0.1.2

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ use clap::{App, Arg};
 
 fn main() {
     let args = App::new("simpleQC")
-        .version("0.1.1")
+        .version("0.1.2")
         .about("Quickly count gc content from a fasta file.")
         .arg(Arg::with_name("input")
             .help("Fastq file to analyze.")
