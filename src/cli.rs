0663586a3a748938eaa85d6e12864dc1708d064f
Heru Handika <herubiolog@gmail.com>
2021-01-07 19:29:27 -0600

    Replace structopt with pure flags. Setup arguments for fastq file processors.

--- src/cli.rs
+++ src/cli.rs
@@ -1,74 +1,99 @@
 //! Heru Handika
-//! 6 January 2020
-//! Module to parse command line
-//! arguments.
-
+//! 
+//! Command line parser
 
 use std::path::PathBuf;
-use structopt::StructOpt;
-// use std::path::Path;
+
+use clap::{App, AppSettings, Arg};
 
 use crate::io;
 
-#[derive(StructOpt)]
-#[structopt(
-    name = "simpleQC",
-    version = "0.1.4",
-    about = "Quickly count gc content from a fasta file.",
-    author = "Heru Handika <hhandi1@lsu.edu>"
-)]
-struct Cli {
-    #[structopt(subcommand)]
-    cmds: Option<Cmd>
-}
-
-
-#[derive(StructOpt)]
-enum Cmd {
-    /// Process fastq files
-    #[structopt(name = "fastq")]
-    Fastq (Opt)
-}
-
-
-#[derive(StructOpt)]
-struct Opt {
-    #[structopt(
-            short= "d", 
-            long="dir",
-            help="Enter path",
-            parse(from_os_str))
-        ]
-        dir: PathBuf,
-
-    // #[structopt(
-    //         short= "wc", 
-    //         long="wildcard",
-    //         help="Use wildcard",
-    //         parse(from_os_str))
-    //     ]
-    //     wildcard: PathBuf
-}
-
-
 pub fn process_fastq_commands(version: &str) {
-    let args = Cli::from_args();
-    
-    if let Some(subcommand) = args.cmds {
-        match subcommand {
-            Cmd::Fastq(opt) => {
-                    let input = PathBuf::from(&opt.dir);
-                    let files = "*.fastq.gz";
-                    let path = input.join(files);
+    let args = App::new("simpleQC")
+                .version("0.1.4")
+                .about("Quickly count gc content from a fasta file")
+                .author("Heru Handika <hhandi1@lsu.edu>")
+                .setting(AppSettings::SubcommandRequiredElseHelp)
+                // Sub command for fastq
+                .subcommand(
+                    App::new("fastq")
+                        .about("Process fastq files")
+                        .arg(
+                            Arg::with_name("dir")
+                                .short("d")
+                                .long("dir")
+                                .help("Process fastq inside a directory")
+                                .conflicts_with_all(&["file", "wildcard", "wdir"])
+                                .takes_value(true)
+                                .value_name("DIR")
+                                // .multiple_values("true"),
+                            )
 
-                    println!("Initiating simpleQC v{}...", version);
-                    io::par_process_inputs(&path);
-                }
-            // Cmd::Fastq(opt) => {
-            //     println!("Initiating simpleQC v{}...", version);
-            //     io::par_process_inputs(&opt.wildcard);
-            // }
-            _ => unreachable!("Fail!"),
-        };
-    }
+                        .arg(
+                            Arg::with_name("file")
+                                .short("f")
+                                .long("file")
+                                .help("Process a single file")
+                                .conflicts_with_all(&[ "dir", "wildcard", "wdir"])
+                                .takes_value(true)
+                                .value_name("FASTQ FILE")
+                            )
+
+                        .arg(
+                            Arg::with_name("wildcard")
+                                .short("c")
+                                .long("wildcard")
+                                .help("Process a single file")
+                                .conflicts_with_all(&[ "dir", "file","wdir"])
+                                .takes_value(true)
+                                .value_name("WILDCARD")
+                            )
+
+                        .arg(
+                            Arg::with_name("wdir")
+                                .short("w")
+                                .long("wdir")
+                                .help("Find all files inside dir and process it.")
+                                .conflicts_with_all(&[ "dir", "file", "wildcard"])
+                                .takes_value(true)
+                                .value_name("PARENT DIR")
+                            )
+                    )
+                .get_matches();
+
+    match args.subcommand() {
+        ("fastq", Some(fastq_matches)) => {
+            if fastq_matches.is_present("dir") {
+                let val = fastq_matches.value_of("dir").unwrap();
+                let input = PathBuf::from(&val);
+                let files = "*.fastq.gz";
+                let path = input.join(files);
+
+                println!("Initiating simpleQC v{}...", version);
+                io::par_process_inputs(&path);
+
+            } else if fastq_matches.is_present("file") {
+                let val = fastq_matches.value_of("file").unwrap();
+                println!("File name {}", &val);
+
+            } else if fastq_matches.is_present("wildcard") {
+                let val = fastq_matches.value_of("wildcard").unwrap();
+                println!("File name {}", &val);
+                
+            } else if fastq_matches.is_present("wdir") {
+                let val = fastq_matches.value_of("wdir").unwrap();
+                println!("File name {}", &val);
+                
+            } else {
+                println!("No command provided!");
+            }
+        }
+        // ("fastq", Some(file_matches)) => {
+        //         if file_matches.is_present("file") {
+        //         let val = file_matches.value_of("file").unwrap();
+        //         println!("File name {}", &val);
+        //     }
+        // }
+        _ => unreachable!("No commands!"),
+    };
 }
