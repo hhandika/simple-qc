6ecd6e5df4bfd3d9206c9e1758832f67b58e623f
Heru Handika <herubiolog@gmail.com>
2021-02-02 11:39:34 -0600

    Refactor code. Merge decoder into one function.

--- src/qscores.rs
+++ src/qscores.rs
@@ -6,37 +6,37 @@ pub struct QScore {
     pub q_len: u32,
     pub mean_q: f64,
     pub low_bases: u32,
-}
-
-#[inline(always)]
-fn decode_qscores(q_line: &[u8]) -> Vec<u32> {
-    q_line.iter()
-        .map(|scr| 
-                { if *scr < 75 {
-                    *scr as u32 - 33
-                } else {
-                    panic!("UNSUPPORTED Q-SCORE ENCODING!");
-                }
-            })
-        .collect()
+    pub sum: u32,
 }
 
 impl QScore {
     pub fn analyze_qscores(q_line: &[u8]) -> Self {
-        let q_scores = decode_qscores(&q_line); 
+        let q_scores = q_line.iter()
+            .map(|scr| 
+                    { if *scr < 75 {
+                        *scr as u32 - 33
+                    } else {
+                        panic!("UNSUPPORTED Q-SCORE ENCODING!");
+                    }
+                })
+            .collect::<Vec<u32>>(); 
 
         let mut q = Self {
                 q_len: q_scores.iter().count() as u32,
-                mean_q: 0.0,
                 low_bases: q_scores.iter()
-                                .filter(|&x| *x < 20)
-                                .count() as u32
+                    .filter(|&x| *x < 20)
+                    .count() as u32,
+                sum: q_scores.iter().sum(),
+                mean_q: 0.0
             };
-        
-        let sum_q: u32 = q_scores.iter().sum();
-        q.mean_q = sum_q as f64 / q.q_len as f64;
+
+        q.mean();
 
         q
+    }
+
+    fn mean(&mut self) {
+        self.mean_q = self.sum as f64 / self.q_len as f64
     }
 }
 
@@ -63,11 +63,11 @@ mod tests {
     #[test]
     fn decode_qscores_test() {
         let q = String::from("II");
-        let qs = decode_qscores(q.as_bytes());
+        let qs = QScore::analyze_qscores(q.as_bytes());
 
-        let res = vec![40, 40];
+        // let res = vec![40, 40];
 
-        assert_eq!(res, qs);
+        assert_eq!(80, qs.sum);
     }
 
     #[test]
@@ -75,6 +75,6 @@ mod tests {
     fn decode_panic_qscore() {
         let p = String::from("II!)K");
 
-        decode_qscores(&p.as_bytes());
+        QScore::analyze_qscores(&p.as_bytes());
     }
 }
